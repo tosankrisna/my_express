2515f3e43c275094661d1a9f56c8df7a7fa4a2bf
tosankrisna <tosankrisna@gmail.com>
2021-06-21 23:10:09 +0800

    update database

--- app/database/my_express.sql
+++ app/database/my_express.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2021 at 06:07 AM
+-- Generation Time: Jun 21, 2021 at 05:09 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -69,40 +69,54 @@ CREATE TABLE `tb_alamat` (
 
 INSERT INTO `tb_alamat` (`id_alamat`, `alamat`, `kecamatan`, `kabupaten`, `provinsi`, `kode_pos`) VALUES
 (12, 'Jalan Bromo', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 80119),
-(54, 'Jalan Gunung Semeru No.1A', 'Blora', 'Kabupaten Blora', 'Jawa Tengah', 78001),
-(55, 'Jalan Sakura No.2', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80115),
-(56, 'Jalan Jupiter No.5', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80556),
-(95, 'Jalan Bung Tomo V No.3', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 80776),
-(97, 'Jalan Gajah No.212', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89010),
-(104, 'Jalan Kaliasem Gang 1', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89001),
-(105, 'Jalan Pulau Sulawesi No.66', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 78990),
-(106, 'Jalan Alam Indah No.231', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89012),
-(107, 'Jalan Pulau Sulawesi No.66', 'Kuta Utara', 'Kota Denpasar', 'Bali', 89122),
-(108, 'Jalan Kaliasem Gang 1', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89101),
-(109, 'Jalan Bangkit 22 No.13', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89013),
-(110, 'Jalan Buluh Indah No.23', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80112),
-(111, 'Jalan Kepundung No.21', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80991),
-(112, 'Jalan Melati No.21', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89770),
-(117, 'Jalan Kaliasem Gang 1', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 80119),
-(118, 'Jalan Kepundung No.21', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80121),
-(119, 'Jalan Lebah Gg.6 No.2', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89002),
-(120, 'Jalan Pulau Sumatera No.112', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80121),
-(121, 'Jalan Buluh Indah No.23', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89012),
-(122, 'Jalan Pulau Sulawesi No.66', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 87121),
-(123, 'Jalan Alam Indah No.231', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80991),
-(124, 'Jalan Pantai Ancol No.1', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80123),
-(125, 'Jalan Melati No.24', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 80991),
-(126, 'Jalan Kamboja No.33', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89112),
-(127, 'Jalan Kaliasem Gang 1', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80121),
-(128, 'Jalan Kepundung No.21', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89010),
-(133, 'Jalan Rambutan No.12', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89901),
-(134, 'Jalan Kepundung No.21', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89012),
-(139, 'Jalan Lebah Gg.6 No.21', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89001),
-(140, 'Jalan Bangkit 2 No.133', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80771),
-(141, 'Jalan Kaliasem Gang 1', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89001),
-(142, 'Jalan Kepundung No.21', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89977),
-(143, 'Jalan Buluh Indah No.23', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89771),
-(144, 'Jalan Pulau Sulawesi No.66', 'Kuta Utara', 'Kota Denpasar', 'Bali', 80978);
+(54, 'Jalan Gunung Semeru', 'Blora', 'Kabupaten Blora', 'Jawa Tengah', 78001),
+(55, 'Jalan Sakura', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80115),
+(56, 'Jalan Jupiter', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80556),
+(95, 'Jalan Bung Tomo', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 80776),
+(97, 'Jalan Gajah', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89010),
+(104, 'Jalan Kaliasem', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89001),
+(105, 'Jalan Pulau Sulawesi', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 78990),
+(106, 'Jalan Alam Indah', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89012),
+(107, 'Jalan Pulau Sulawesi', 'Kuta Utara', 'Kota Denpasar', 'Bali', 89122),
+(108, 'Jalan Kaliasem', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89101),
+(109, 'Jalan Bangkit', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89013),
+(110, 'Jalan Buluh Indah', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80112),
+(111, 'Jalan Kepundung', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80991),
+(112, 'Jalan Melati', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89770),
+(117, 'Jalan Kaliasem', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 80119),
+(118, 'Jalan Kepundung', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80121),
+(119, 'Jalan Alpukat', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89002),
+(120, 'Jalan Pulau Sumatera', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80121),
+(121, 'Jalan Buluh Indah', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89012),
+(122, 'Jalan Pulau Sulawesi', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 87121),
+(123, 'Jalan Alam Indah', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80991),
+(124, 'Jalan Pantai Ancol', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80123),
+(125, 'Jalan Melati', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 80991),
+(126, 'Jalan Kamboja', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89112),
+(127, 'Jalan Kaliasem', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80121),
+(128, 'Jalan Kepundung', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89010),
+(133, 'Jalan Rambutan', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89901),
+(134, 'Jalan Kepundung', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 89012),
+(139, 'Jalan Lebah', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89001),
+(140, 'Jalan Bangkit', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 80771),
+(141, 'Jalan Kaliasem', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89001),
+(142, 'Jalan Kepundung', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89977),
+(143, 'Jalan Buluh Indah', 'Denpasar Utara', 'Kota Denpasar', 'Bali', 89771),
+(144, 'Jalan Pulau Sulawesi', 'Kuta Utara', 'Kota Denpasar', 'Bali', 80978),
+(145, 'Jalan Sekar Mas', 'Legian', 'Kabupaten Badung', 'Bali', 89761),
+(146, 'Jalan Kecapi', 'Bangkalan', 'Kabupaten Bangkalan', 'Madura', 65411),
+(147, 'Jalan Patih Jelantik', 'Kuta Selatan', 'Kabupaten Badung', 'Bali', 88771),
+(148, 'Jalan Kepulauan Seribu', 'Jatiluwih', 'Kabupaten Tabanan', 'Bali', 87660),
+(149, 'Jalan Pulau Saelus', 'Denpasar Selatan', 'Kota Denpasar', 'Bali', 89911),
+(150, 'Jalan Pantai Melasti', 'Canggu', 'Kabupaten Badung', 'Bali', 80172),
+(151, 'Jalan Mawar Merah', 'Mengwi', 'Kabupaten Badung', 'Bali', 54112),
+(152, 'Jalan Kecubung', 'Denpasar Timur', 'Kota Denpasar', 'Bali', 80997),
+(153, 'Jalan Pulau Samosir', 'Ubud', 'Kabupaten Gianyar', 'Bali', 78990),
+(154, 'Jalan Haji Rohma', 'Banjarangkan', 'Kabupaten Klungkung', 'Bali', 80011),
+(157, 'Jalan Melati', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89911),
+(158, 'Jalan Pantai Kedonganan', 'Kedonganan', 'Kabupaten Badung', 'Bali', 89112),
+(159, 'Jalan Nusantara', 'Denpasar Barat', 'Kota Denpasar', 'Bali', 89112),
+(160, 'Jalan Kusuma Bangsa', 'Sembung', 'Kabupaten Badung', 'Bali', 876101);
 
 -- --------------------------------------------------------
 
@@ -218,7 +232,14 @@ INSERT INTO `tb_paket` (`id_paket`, `nama_paket`, `berat_paket`, `jenis_paket`, 
 (40, 'Monitor LG 24inch', 2.00, 'normal', 'bubble wrap', 'regular', '32500', NULL, 15, 43, 41, 23),
 (42, 'Kursi Gaming Logitech', 2.00, 'normal', 'bubble wrap', 'regular', '32500', NULL, 15, 45, 43, 23),
 (43, 'Laptop Acer', 1.00, 'normal', 'bubble wrap', 'regular', '25000', 5, 15, 46, 44, NULL),
-(44, 'Topi', 2.00, 'normal', 'bubble wrap', 'regular', '32500', 5, 18, 47, 45, NULL);
+(44, 'Topi', 2.00, 'normal', 'bubble wrap', 'regular', '32500', 5, 18, 47, 45, NULL),
+(45, 'Sepeda Polygon', 2.00, 'fragile', 'kayu', 'ekonomi', '50000', 5, 18, 48, 46, NULL),
+(46, 'Bibit Anggur Merah', 0.40, 'normal', 'bubble wrap', 'regular', '20500', 5, 18, 49, 47, NULL),
+(47, 'HP Xiaomi Redmi Note 8', 1.20, 'fragile', 'bubble wrap', 'regular', '26500', 5, 18, 50, 48, NULL),
+(48, 'Gitar Gibson SG Standard', 3.50, 'fragile', 'kayu', 'ekonomi', '61250', 5, 18, 51, 49, NULL),
+(49, 'Jersey Bola', 0.80, 'normal', 'bubble wrap', 'regular', '23500', 5, 18, 52, 50, NULL),
+(51, 'HP Samsung S10', 2.00, 'normal', 'bubble wrap', 'ekonomi', '27500', 5, 15, 54, 52, NULL),
+(52, 'Iphone X', 1.40, 'normal', 'bubble wrap', 'super kilat', '33000', 5, 15, 55, 53, NULL);
 
 -- --------------------------------------------------------
 
@@ -251,7 +272,14 @@ INSERT INTO `tb_penerima` (`id_penerima`, `nama_penerima`, `jenis_kelamin`, `no_
 (43, 'Putu Dede', 'laki-laki', '089771656777', 134),
 (45, 'Kadek Ana', 'perempuan', '087761555232', 140),
 (46, 'Putu Dede', 'laki-laki', '089777112222', 142),
-(47, 'Rahmat Hidayat', 'laki-laki', '089766512333', 144);
+(47, 'Rahmat Hidayat', 'laki-laki', '089766512333', 144),
+(48, 'Gilang Gunawan', 'laki-laki', '087661545123', 146),
+(49, 'Ryan Ryanto', 'laki-laki', '081771212222', 148),
+(50, 'Gung Krisna', 'laki-laki', '087661778909', 150),
+(51, 'Komang Setiary', 'perempuan', '087112665432', 152),
+(52, 'Aldy Septian', 'laki-laki', '081662321444', 154),
+(54, 'Komang Gede', 'laki-laki', '087661546112', 158),
+(55, 'Bambang', 'laki-laki', '089771654212', 160);
 
 -- --------------------------------------------------------
 
@@ -284,7 +312,14 @@ INSERT INTO `tb_pengirim` (`id_pengirim`, `nama_pengirim`, `no_telp`, `jenis_kel
 (41, 'Putra', '089771656443', 'laki-laki', 133),
 (43, 'Aldo', '087761555444', 'laki-laki', 139),
 (44, 'Ahmad Bachtiar', '089776554656', 'laki-laki', 141),
-(45, 'Putu Andika Putra', '081667554654', 'laki-laki', 143);
+(45, 'Putu Andika Putra', '081667554654', 'laki-laki', 143),
+(46, 'Putra Sentana', '087661543212', 'laki-laki', 145),
+(47, 'Agung Prawira', '089776121333', 'laki-laki', 147),
+(48, 'Prabawati', '089112737661', 'perempuan', 149),
+(49, 'Gangga Permata Sari', '087991821334', 'perempuan', 151),
+(50, 'Berliana', '087112631666', 'perempuan', 153),
+(52, 'Bunga', '089771634555', 'perempuan', 157),
+(53, 'Agus Junaedi', '081772612111', 'laki-laki', 159);
 
 -- --------------------------------------------------------
 
@@ -318,12 +353,19 @@ INSERT INTO `tb_tracking` (`id_tracking`, `status_paket`, `keterangan`, `no_resi
 (32, 'gagal dikirim', '-', '5i4s3IF2vs5', 'P1520210618', '2021-06-15 09:09:32', NULL, 34, 37, 15, 35),
 (33, 'selesai dikirim', '-', 'pbkWOVzsLKd', 'P1520210617', '2021-06-15 09:27:49', '2021-06-15 09:28:13', 35, 38, 15, 36),
 (34, 'selesai dikirim', '-', 'D6SwaLUAcqg', 'P1520210618', '2021-06-19 02:35:39', '2021-06-19 02:35:48', 36, 39, 15, 37),
-(35, 'sedang dikirim', '-', 'dPm7lg9UfFb', 'P1520210618', '2021-06-21 02:47:07', NULL, 37, 40, 15, 38),
-(36, 'sedang dikirim', '-', '1SzuGpWLs1f', 'P1520210618', '2021-06-21 02:47:55', NULL, 38, 41, 15, 39),
-(38, 'sedang dikirim', '-', 'IobFD4cddwz', 'P1520210618', '2021-06-21 02:47:57', NULL, 40, 43, 15, 41),
+(35, 'selesai dikirim', '-', 'dPm7lg9UfFb', 'P1520210618', '2021-06-21 02:47:07', '2021-06-21 19:52:00', 37, 40, 15, 38),
+(36, 'terlambat dikirim', '-', '1SzuGpWLs1f', 'P1520210618', '2021-06-01 02:47:55', '2021-06-21 19:51:36', 38, 41, 15, 39),
+(38, 'terlambat dikirim', '-', 'IobFD4cddwz', 'P1520210618', '2021-06-08 02:47:57', '2021-06-21 19:42:52', 40, 43, 15, 41),
 (40, 'selesai dikirim', '-', 'SZwMCRqXl6g', 'P1520210617', '2021-06-15 10:23:32', '2021-06-15 10:23:51', 42, 45, 15, 43),
-(41, 'sedang dikirim', '-', 'czylLRw9Z7E', 'P1520210618', '2021-06-21 02:47:58', NULL, 43, 46, 15, 44),
-(42, 'belum dikirim', '-', 'TVB2ArPilPJ', 'P1820210618', NULL, NULL, 44, 47, 18, 45);
+(41, 'terlambat dikirim', '-', 'czylLRw9Z7E', 'P1520210618', '2021-06-15 02:47:58', '2021-06-21 19:39:41', 43, 46, 15, 44),
+(42, 'sedang dikirim', '-', 'TVB2ArPilPJ', 'P1820210618', '2021-06-21 22:41:24', NULL, 44, 47, 18, 45),
+(43, 'selesai dikirim', '-', 'eT27UTUDC1I', 'P1820210621', '2021-06-21 22:41:38', '2021-06-21 22:42:28', 45, 48, 18, 46),
+(44, 'selesai dikirim', '-', 'I8gTKD8Tiqq', 'P1820210621', '2021-06-21 22:41:40', '2021-06-21 22:42:21', 46, 49, 18, 47),
+(45, 'gagal dikirim', '-', 'k0O4QKLrpGD', 'P1820210621', '2021-06-21 22:41:41', NULL, 47, 50, 18, 48),
+(46, 'gagal dikirim', '-', 'M6zuJN5lzw3', 'P1820210621', '2021-06-21 22:41:43', NULL, 48, 51, 18, 49),
+(47, 'gagal dikirim', '-', 'IaTB0g029zn', 'P1820210621', '2021-06-21 22:41:45', NULL, 49, 52, 18, 50),
+(49, 'belum dikirim', '-', '8ZMLePdsCHL', 'P1520210621', NULL, NULL, 51, 54, 18, 52),
+(50, 'belum dikirim', '-', 'z6FocACQPRS', 'P1520210621', NULL, NULL, 52, 55, 18, 53);
 
 --
 -- Indexes for dumped tables
@@ -414,7 +456,7 @@ ALTER TABLE `tb_admin`
 -- AUTO_INCREMENT for table `tb_alamat`
 --
 ALTER TABLE `tb_alamat`
-  MODIFY `id_alamat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
+  MODIFY `id_alamat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=161;
 
 --
 -- AUTO_INCREMENT for table `tb_cs`
@@ -438,25 +480,25 @@ ALTER TABLE `tb_kurir`
 -- AUTO_INCREMENT for table `tb_paket`
 --
 ALTER TABLE `tb_paket`
-  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `tb_penerima`
 --
 ALTER TABLE `tb_penerima`
-  MODIFY `id_penerima` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id_penerima` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `tb_pengirim`
 --
 ALTER TABLE `tb_pengirim`
-  MODIFY `id_pengirim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id_pengirim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `tb_tracking`
 --
 ALTER TABLE `tb_tracking`
-  MODIFY `id_tracking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_tracking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- Constraints for dumped tables
